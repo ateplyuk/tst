5e6454d65df164c80944336dcad7df04f58d1872
AlexanderTeplyuk <aleksandrtepliuk@gmail.com>
2022-05-11 10:28:41 +0300

    st5

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+requirements = read('requirements.txt').splitlines()
+
 setup(name='tst',
       version='1.0.0',
       description='test',
       url='https://github.com/ateplyuk/tst'
-      packages=['tst']
+      packages=['tst'],
+      install_requires=requirements
       )
