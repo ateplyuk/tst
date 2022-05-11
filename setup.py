d15309e7f8731e834b5922346685b5f006417ee7
AlexanderTeplyuk <aleksandrtepliuk@gmail.com>
2022-05-11 10:32:55 +0300

    st7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = read('requirements.txt').splitlines()
 setup(name='tst',
       version='1.0.0',
       description='test',
-      url='https://github.com/ateplyuk/tst'
+      url='https://github.com/ateplyuk/tst',
       packages=['tst'],
       install_requires=requirements
       )
